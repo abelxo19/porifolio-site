13491e6100a648032b602627a5a47169445b647a
abel <abelaatkelet@gmail.com>
2025-03-09 14:11:14 +0300

    modify-padding-about-section

--- components/about.tsx
+++ components/about.tsx
@@ -27,7 +27,7 @@ export default function About() {
   }, []);
 
   return (
-    <section id="about" className="py-20">
+    <section id="about" className="py-11 md:py-20">
       <div className="container px-4 md:px-6">
         <div className="grid gap-6 lg:grid-cols-2 lg:gap-12 items-center">
           {/* 3D Effect Image */}
